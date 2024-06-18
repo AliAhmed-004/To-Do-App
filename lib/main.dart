6289f3e12dc79aa6ffd1a6cef889f83534853ddf
AliAhmed-004 <eliehmed908@gmail.com>
2024-06-18 15:55:20 +0500

    Updated dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'To Do',
       theme: ThemeData(primaryColor: Colors.yellow),
       debugShowCheckedModeBanner: false,
       home: const HomePage(),
